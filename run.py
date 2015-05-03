6d91ac7492aa2c443a9ed8b46463026b44ae7efc
Curtis Vogt <curtis.vogt@invenia.ca>
2015-05-03 09:02:48 -0500

    Fixed debug/reload flags.
    The run parameters use_debugger/use_reloader require debug to be True
    in order for the other to to be respected.
    
    http://flask.pocoo.org/docs/0.10/errorhandling/#working-with-debuggers

--- run.py
+++ run.py
@@ -26,6 +26,10 @@ if __name__ == '__main__':
                         action="store_true")
     args = parser.parse_args()
 
-    app.run(host="0.0.0.0" if args.public else "localhost", port=args.port,
-            use_debugger=args.debug, use_reloader=args.reload)
-
+    app.run(
+        host="0.0.0.0" if args.public else "localhost",
+        port=args.port,
+        debug=args.debug or args.reload,
+        use_debugger=args.debug,
+        use_reloader=args.reload,
+    )
