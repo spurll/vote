19e3b0cc59644bbb4185635a9c8dac34a540f084
Gem Newman <spurll@gmail.com>
2015-08-28 15:05:25 -0500

    Added a basic test case for random sample.

--- tests/test_selection.py
+++ tests/test_selection.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 
 from vote import selection
 
@@ -13,48 +14,58 @@ class TestObject(object):
         self.premium = premium
 
 
-primitive_data = [
-    [0, 3, 2],
-    [0, 1, 3, 2, 4],
-    [2],
-    [1, 0],
-    [0],
-    [0, 1, 2, 4, 3, 5, 6, 7, 8],
-    [5, 0],
-    [6, 0],
-    [7, 2],
-    [8],
-]
-
-
-
-# Add super basic tests with just one winner (called with default number of
-# winners).
+# Premium limits are never tested. Oh well.
 
 
 class TestWeightedSample(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestWeightedSample, self).__init__(*args, **kwargs)
         self.selection = selection.weighted_sample
+        self.data = [
+            [0],
+            [0, 1],
+            [0, 1, 2],
+            [0, 1, 2, 3],
+            [1, 2, 3, 4],
+            [2, 3, 4, 5],
+            [3, 4, 5, 6],
+            [4, 5, 6, 7, 8],
+            [0, 1, 2, 3, 4, 5, 6, 7, 8],
+            [0, 1, 2, 3, 4, 5, 6, 8, 7],
+        ]
+        self.expected = list(range(9))
 
     def test_basic(self):
-        pass
+        # Select 10000 winners. The test is designed such that the lower values
+        # should consistently be selected more often.
+        winners = [self.selection(self.data)[0] for i in range(10000)]
+        counter = Counter(winners)
+        most_common = [key for key, value in counter.most_common()]
 
-    def test_premium_limit(self):
-        # Basic testing with premium limits.
-
-        # Test with primitives (no premium limit attributes).
-        pass
+        self.assertEqual(most_common, self.expected)
 
 
 class TestInstantRunoff(unittest.TestCase):
     def __init__(self, *args, **kwargs):
         super(TestInstantRunoff, self).__init__(*args, **kwargs)
         self.selection = selection.instant_runoff
+        self.data = [
+            [0, 3, 2],
+            [0, 1, 3, 2, 4],
+            [2],
+            [1, 0],
+            [0],
+            [0, 1, 2, 4, 3, 5, 6, 7, 8],
+            [5, 0],
+            [6, 0],
+            [7, 2],
+            [8],
+        ]
+        self.expected = list(range(9))
 
     def test_basic(self):
-        expected = list(range(9))
-        self.assertEqual(self.selection(primitive_data, winners=9), expected)
+        self.assertEqual(self.selection(self.data), [self.expected[0]])
+        self.assertEqual(self.selection(self.data, winners=9), self.expected)
 
 
 if __name__ == '__main__':
